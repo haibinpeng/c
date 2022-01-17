8ed7a39643b9e695b0f2a3fa8e47e2e17ee74153
Haibin Peng <916250727@qq.com>
2022-01-17 21:24:57 +0800

    woniule

---
+++ makefile/func.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void print()
+{
+	printf("I am print func\n");
+	printf("I am studying makefile\n");
+}
---
+++ makefile/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void print();
+int sub(int,int);
+int main()
+{
+	printf("hello world\n");
+	print();
+	printf("5-3=%d\n",sub(5,3));
+	return 0;
+}
