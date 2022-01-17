9d378d6a2a9ad400d27b38adfaba0cb1ac6b7118
Haibin Peng <916250727@qq.com>
2022-01-17 21:16:01 +0800

    woniule

---
+++ fopen/fopen_ab.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	FILE *fp=fopen("file","ab+");
+	if(NULL==fp)
+	{
+		perror("fopen");
+		return -1;
+	}
+	//fwrite("world",sizeof(char),5,fp);
+	char buf[10]={0};
+	fread(buf,1,5,fp);
+	puts(buf);
+	return 0;
+}
