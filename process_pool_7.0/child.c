984c22f264376da964010e825a0b2b6107a654d7
Haibin Peng <916250727@qq.com>
2022-02-19 23:46:06 +0800

    woniule

---
+++ process_pool_7.0/child.c
@@ -0,0 +1,40 @@
+#include "process_pool.h"
+
+int make_child(process_data_t *p_manage,int process_num)
+{
+    int i;
+    pid_t pid;
+    int fds[2];//与子进程之间的管道
+    int ret;
+    for(i=0;i<process_num;i++)//创建多个子进程
+    {
+        socketpair(AF_LOCAL,SOCK_STREAM,0,fds);//全双工管道
+        pid=fork();
+        if(0==pid)//子进程
+        {
+            close(fds[1]);//子进程关闭fds[1]
+            child_handle(fds[0]);//一定要让子进程在child_handle内exit
+        }
+        close(fds[0]);//父进程关闭fds[0],通过fds[1]和子进程进行通信,和pipe的区别是fds[1]即可读也可写
+        p_manage[i].pid=pid;
+        p_manage[i].fd=fds[1];
+        p_manage[i].busy=0;//默认子进程是非忙碌状态
+#ifdef DEBUG
+        printf("p_manage[%d].fd=%d\n",i,p_manage[i].fd);
+#endif
+    }
+}
+
+int child_handle(int fd)
+{
+    int new_fd,ret;//子进程要接收一个new_fd,通过new_fd给对应客户端传文件
+    char flag=1;
+    while(1)
+    {
+        recv_fd(fd,&new_fd);//接受任务
+        printf("gets task,new_fd=%d\n",new_fd);
+        trans_file(new_fd);//发文件给客户端
+        close(new_fd);
+        write(fd,&flag,sizeof(flag));//通知父进程任务完成
+    }
+}
---
+++ process_pool_7.0/trans_file.c
@@ -0,0 +1,56 @@
+#include "process_pool.h"
+
+int trans_file(int new_fd)
+{
+    int fd=open(FILENAME,O_RDONLY);
+    ERROR_CHECK(fd,-1,"open")
+    int ret;
+    //发送文件名
+    train_t t;
+    t.data_len=strlen(FILENAME);//文件名长度
+    strcpy(t.buf,FILENAME);//文件名
+    ret=send_n(new_fd,&t,4+t.data_len);//4是火车头长度
+    SELFFUNC_ERR_CHECK(ret,"send_n1")
+    //发送文件大小
+    struct stat buf;
+    ret=stat(FILENAME,&buf);
+    ERROR_CHECK(ret,-1,"stat")
+    t.data_len=sizeof(buf.st_size);
+    memcpy(t.buf,&buf.st_size,sizeof(buf.st_size));
+    ret=send_n(new_fd,&t,4+t.data_len);
+    SELFFUNC_ERR_CHECK(ret,"send_n2")
+    //发送文件内容,使用sendfile
+    ret=sendfile(new_fd,fd,0,buf.st_size);
+    ERROR_CHECK(ret,-1,"sendfile")
+    return 0;
+}
+
+int recv_n(int sfd,void* buf,int data_len)
+{
+    //循环接收数据
+    char *p=(char *)buf;
+    int total=0,ret;
+    while(total<data_len)
+    {
+        ret=recv(sfd,p+total,data_len-total,0);
+        total+=ret;
+    }
+    return 0;
+}
+
+int send_n(int sfd,void* buf,int data_len)
+{
+    //循环发送数据
+    char *p=(char *)buf;
+    int total=0,ret;
+    while(total<data_len)
+    {
+        ret=send(sfd,p+total,data_len-total,0);
+        if(-1==ret)//客户端断开
+        {
+            return -1;
+        }
+        total+=ret;
+    }
+    return 0;
+}
